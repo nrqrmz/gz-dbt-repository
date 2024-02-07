5ec724f344888ad66f134e9d2ba54a4c07c04a74
Enrique Ramirez Ramos <nrqrmz@gmail.com>
2024-02-07 07:21:09 +0000

    add margin and purchase cost

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,10 @@
+with
+    joined as (
+        select *
+        from {{ ref("stg_raw__sales") }}
+        left join {{ ref("stg_raw__product") }} using (products_id)
+    ),
+    renamed as (select *, round(quantity * cast(purchase_price as float64), 2) as purchase_cost from joined)
+
+select *, round(revenue - purchase_cost,2) as margin
+from renamed
